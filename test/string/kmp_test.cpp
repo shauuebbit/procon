571db7b3d4a7f6489c57576cad7727faf1d5025b
shauuebbit <shauuebbit@outlook.com>
2022-09-12 03:09:33 +0900

    Add KMP std::vector ver.

--- test/string/kmp_test.cpp
+++ test/string/kmp_test.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-TEST(KMPTest, SmallSearch) {
+TEST(KMPTest, SmallSearch1) {
     const string target = "aabacabbaaababbabaabbbbaba";
 
     for (int l = 0; l < target.size(); l++) {
@@ -22,5 +22,23 @@ TEST(KMPTest, SmallSearch) {
 
     for (const string& pattern : {"d", "abababababab", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"}) {
         ASSERT_EQ(search(target, pattern), -1);
+    }
+}
+
+TEST(KMPTest, SmallSearch2) {
+    const vector<int> target = {1, 0, 1, 0, 1, 1, 0, 0, 1, 2, 0, 1, 2, 0, 1, 1, 0, 0, 1, 0, 1, 0};
+
+    for (int l = 0; l < target.size(); l++) {
+        for (int r = l + 1; r <= target.size(); r++) {
+            vector<int> pattern(r - l);
+            for (int i = l; i < r; i++) pattern[i - l] = target[i];
+
+            int pos = search(target, pattern);
+            ASSERT_GE(pos, 0);
+            ASSERT_LE(pos, l);
+
+            pattern.push_back(10);
+            ASSERT_EQ(search(target, pattern), -1);
+        }
     }
 }
