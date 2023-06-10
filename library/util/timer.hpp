15ead4866eaa38804b6971fbb3fea062cb81a9b7
shauuebbit <shauuebbit@outlook.com>
2023-06-10 23:10:46 +0900

    Add elapse

--- library/util/timer.hpp
+++ library/util/timer.hpp
@@ -17,14 +17,35 @@ class Timer {
         time_stack.push(t);
     }
 
-    std::chrono::system_clock::duration stop() {
+    std::chrono::system_clock::duration elapse() {
         auto e = std::chrono::system_clock::now() - time_stack.top();
+        return e;
+    }
+
+    std::chrono::system_clock::duration stop() {
+        auto e = this->elapse();
         time_stack.pop();
         return e;
     }
 
+    long double elapse_as_nanoseconds() {
+        return std::chrono::duration_cast<std::chrono::nanoseconds>(this->elapse()).count();
+    }
+
+    long double elapse_as_microseconds() {
+        return std::chrono::duration_cast<std::chrono::microseconds>(this->elapse()).count();
+    }
+
+    long double elapse_as_milliseconds() {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(this->elapse()).count();
+    }
+
+    long double elapse_as_seconds() {
+        return std::chrono::duration_cast<std::chrono::seconds>(this->elapse()).count();
+    }
+
     long double stop_as_nanoseconds() {
-        return std::chrono::duration_cast<std::chrono::minutes>(stop()).count();
+        return std::chrono::duration_cast<std::chrono::nanoseconds>(stop()).count();
     }
 
     long double stop_as_microseconds() {
