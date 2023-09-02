c6777397996fdddc9b820b053617c8d369aed435
shauuebbit <shauuebbit@outlook.com>
2023-09-02 15:32:35 +0900

    Change int to size_t

--- library/data_structure/union_find.hpp
+++ library/data_structure/union_find.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <vector>
 
 class UnionFind {
@@ -10,9 +11,12 @@ class UnionFind {
    public:
     UnionFind(size_t n = 0) : data(n, -1), sz(n) {}
 
-    int find(int x) { return data[x] < 0 ? x : data[x] = find(data[x]); }
+    size_t find(size_t x) {
+        assert(x < data.size());
+        return static_cast<size_t>(data[x] < 0 ? x : data[x] = find(data[x]));
+    }
 
-    bool unite(int x, int y) {
+    bool unite(size_t x, size_t y) {
         x = find(x);
         y = find(y);
 
@@ -27,11 +31,9 @@ class UnionFind {
         }
     }
 
-    bool is_same(int x, int y) { return find(x) == find(y); }
+    bool is_same(size_t x, size_t y) { return find(x) == find(y); }
 
-    size_t size(int x) {
-        return static_cast<size_t>(-data[find(x)]);
-    }
+    size_t size(size_t x) { return static_cast<size_t>(-data[find(x)]); }
 
-    size_t size() { return sz; }
+    size_t size() const { return sz; }
 };
