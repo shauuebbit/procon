264edd067f19b177cd6b5da1ebc3af2de1ebb640
shauuebbit <shauuebbit@outlook.com>
2024-03-10 17:02:01 +0900

    Add const

--- library/geometry/arg_sort.hpp
+++ library/geometry/arg_sort.hpp
@@ -6,8 +6,8 @@
 #include "point.hpp"
 
 template <typename T>
-void arg_sort(std::vector<Point<T>>& points) {
-    auto o = [](const Point<T>& p) {
+constexpr void arg_sort(std::vector<Point<T>>& points) {
+    constexpr auto ord = [](const Point<T>& p) {
         if (p.y() > 0)
             return 1;
         else if (p.y() < 0)
@@ -18,9 +18,9 @@ void arg_sort(std::vector<Point<T>>& points) {
             return 0;
     };
 
-    std::sort(points.begin(), points.end(), [&o](const Point<T>& p0, const Point<T>& p1) {
-        auto o0 = o(p0);
-        auto o1 = o(p1);
+    std::ranges::sort(points, [&ord = std::as_const(ord)](const Point<T>& p0, const Point<T>& p1) {
+        auto o0 = ord(p0);
+        auto o1 = ord(p1);
         if (o0 != o1)
             return o0 < o1;
         else if (o0 == 1)
