34938a5b4d5e6e31f24a9b7e670bdf433e553683
shauuebbit <shauuebbit@outlook.com>
2024-03-23 17:22:32 +0900

    Add extgcd

--- library/math/gcd_lcm.hpp
+++ library/math/gcd_lcm.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <tuple>
+
 template <typename T, typename U>
 constexpr T gcd(T a, U b) {
     while (b) {
@@ -14,4 +16,34 @@ constexpr T gcd(T a, U b) {
 template <typename T, typename U>
 constexpr T lcm(T a, U b) {
     return a / gcd(a, b) * b;
+}
+
+template <typename T, typename U>
+constexpr std::tuple<T, T, T> extgcd(T a, U b) {
+    if (a == 0) return {0, 1, b};
+    else if (b == 0) return {1, 0, a};
+
+    std::tuple<T, T, T> aa = {1, 0, a};
+    std::tuple<T, T, T> bb = {0, 1, b};
+    std::tuple<T, T, T> tt;
+
+    constexpr auto add = [](const std::tuple<T, T, T>& lhs, const std::tuple<T, T, T>& rhs) -> std::tuple<T, T, T> {
+        auto [lhs0, lhs1, lhs2] = lhs;
+        auto [rhs0, rhs1, rhs2] = rhs;
+        return {lhs0 + rhs0, lhs1 + rhs1, lhs2 + rhs2};
+    };
+
+    constexpr auto mul = [](const std::tuple<T, T, T>& lhs, const T& rhs) -> std::tuple<T, T, T> {
+        auto [lhs0, lhs1, lhs2] = lhs;
+        return {lhs0 * rhs, lhs1 * rhs, lhs2 * rhs};
+    };
+
+    while (std::get<2>(bb) != 0) {
+        T w = std::get<2>(aa) / std::get<2>(bb);
+        tt = add(aa, mul(bb, -w));
+        aa = bb;
+        bb = tt;
+    }
+
+    return aa;
 }
--- test/math/gcd_lcm_test.cpp
+++ test/math/gcd_lcm_test.cpp
@@ -13,3 +13,15 @@ TEST(GCDLCMTest, LCM) {
     ASSERT_EQ(lcm(2, 0), 0);
     ASSERT_EQ(lcm(4, 1), 4);
 }
+
+TEST(GCDLCMTest, ExtGCD) {
+    constexpr auto test = [](int a, int b) {
+        auto [x, y, g] = extgcd(a, b);
+        ASSERT_EQ(a * x + b * y, g);
+        ASSERT_EQ(g, gcd(a, b));
+    };
+
+    test(12, 18);
+    test(2, 0);
+    test(4, 1);
+}
